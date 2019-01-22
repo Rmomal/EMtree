e9fbb809d59acb4b63290c04ba2e0886d4eca721
Rmomal <rmomaleisenring@gmail.com>
2019-01-22 11:05:01 +0100

    new functions

---
+++ R/EMtree.R
@@ -0,0 +1,38 @@
+
+
+##############
+# DATA
+##############
+#data_from_scratch: generate data under the PLN model with a certain type of dependency structure,
+#                   and draw the latter structure if asked.
+# type: type of graph, either "tree", "erdos", "cluster" or "scale-free"
+# p: wanted number of columns (species)
+# covar: covariates
+# prob: edge probability for erdos graphs
+# dens: density of edges for cluster graphs
+# r: within/between connectiviy ratio for cluster graphs
+
+data_from_scratch<-function(type, p=20, r=5, covar,prob=log(p)/p,dens=log(p)/p, draw=FALSE){
+  # make graph
+  graph<- generator_graph(graph=type,d=p,prob=prob,dens=dens,r=r)
+  param<-generator_param(as.matrix(graph))
+  data<-generator_PLN(param$sigma,covar)[[1]]
+  if(draw){ as_tbl_graph(as.matrix(graph)) %>%
+      ggraph(layout="kk")+
+      geom_edge_link()+
+      geom_node_point(size=3, color="blue")
+  }
+  return(list(data=data,omega= param$omega))
+}
+
+
+##############
+# INFERENCES
+##############
+EMtree<-function(Y=data,covariates){
+  PLN = PLN(Y ~ -1+covariates)                  # run PLN
+  Sigma<-PLN$model_par$Sigma
+  corVEM<-cov2cor(Sigma)
+  inf_treeggm<-TreeGGM(corVEM,"FALSE",FALSE)$P  # run EM
+  return(inf_treeggm)
+}
---
+++ R/FunctionsTree.R
@@ -0,0 +1,101 @@
+######################################################################################
+# Summing over trees (Matrix-tree thm)
+Laplacian <- function(W){
+   # W = squared weight matrix
+   return(diag(rowSums(W)) - W)
+}
+
+######################################################################################
+# Summing over trees (Matrix-tree thm)
+SumTree <- function(W){
+   # W = squared weight matrix
+   return(det(Laplacian(W)[-1, -1]))
+}
+
+######################################################################################
+# Summing over trees (Matrix-tree thm)
+LogSumTree <- function(W){
+   # W = squared weight matrix
+   return(sum(log(eigen(Laplacian(W)[-1, -1], symmetric=T)$values)))
+}
+
+######################################################################################
+# Computing edge probability
+Kirshner <- function(W){
+   # W = squared weight matrix
+   # Kirshner (07) formulas
+   # W = beta.unif*phi
+
+   if(!isSymmetric(W)){cat('Pb: W non symmetric!')}
+   p = nrow(W)
+   L = Laplacian(W)[-1, -1]
+   Leigen = eigen(L); Q = (Leigen$vectors) %*% diag(1/Leigen$values) %*% t(Leigen$vectors)
+   # Q = chol2inv(chol(L));
+   # Q = solve(L[-1, -1]);
+   # P = W[-1, -1] * (diag(Q)%o%rep(1, p-1) + rep(1, p-1)%o%diag(Q) - 2*Q)
+   # P = rbind(c(0, W[1, -1]*diag(Q)), cbind(W[-1, 1]*diag(Q), P))
+   Q = rbind(c(0, diag(Q)),
+                 cbind(diag(Q), (diag(Q)%o%rep(1, p-1) + rep(1, p-1)%o%diag(Q) - 2*Q)))
+   Q = .5*(Q + t(Q))
+   P = W * Q
+   P = .5*(P + t(P))
+   return(list(P=P, Q=Q))
+}
+
+KirshnerRM <- function(beta){
+   L = Laplacian(beta)[-1, -1]
+   # Q<-solve(L)
+   Leigen = eigen(L); Q = (Leigen$vectors) %*% diag(1/Leigen$values) %*% t(Leigen$vectors)
+   colQ<-matrix(diag(Q),nrow=length(diag(Q)),ncol=length(diag(Q)),byrow=TRUE)
+   rowQ<-matrix(diag(Q),nrow=length(diag(Q)),ncol=length(diag(Q)),byrow=FALSE)
+   matrixM<-(colQ+rowQ-2*Q)
+   matrixM<-rbind(diag(Q),matrixM)
+   matrixM<-cbind(c(0,diag(Q)),matrixM)
+   matrixK<-beta*matrixM
+   # matrix<-beta*Meila(beta)
+   return(list(P=matrixK,Q=matrixM))
+}
+
+   ######################################################################################
+# Computing edge probability
+EdgeProba <- function(W){
+   # W = squared weight matrix
+   # Direct calculation
+   if(!isSymmetric(W)){cat('Pb: W non symmpetric!')}
+   p = nrow(W); P = matrix(0, p, p); Wcum = SumTree(W)
+
+   # cst=1 ;
+   # while(min(eigen(W)$values)<0){
+   #   W=W+diag(rep(cst,ncol(W)));
+   #   cst=1.1*cst
+   #  }
+  #  browser()
+   sapply(1:(p-1),
+          function(j){
+            sapply((j+1):p,
+              function(k){
+                W_jk = W; W_jk[j, k] = W_jk[k, j] = 0 #tuer l'arête kj dans W_kj
+                P[k, j] <<- 1 - SumTree(W_jk) / Wcum
+                P[j, k] <<- P[k, j]
+              }
+            )
+          }
+    )
+   return(P)
+}
+
+
+######################################################################################
+# Corrected edge probability, depending on the prior
+CorrectedEdgeProba <- function(Pedge, p0=matrix(1/ncol(Pedge), nrow(Pedge), ncol(Pedge)), p=matrix(1/2, nrow(Pedge), ncol(Pedge))){
+   return(Pedge*p/p0 / (Pedge*p/p0 + (1-Pedge)*(1-p)/(1-p0)))
+}
+
+######################################################################################
+# Uniform sampling of a spanning tree
+rSpanTree <- function(p){
+   A = get.adjacency(mst(graph.adjacency(matrix(runif(p^2), p, p), weighted=TRUE, mode='upper')))
+   # A = A + diag(rep(1, p))
+   return(as.matrix(A))
+}
+
