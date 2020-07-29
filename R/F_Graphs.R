f00f85fa4613c4adefed196b6e23ac0d4fb6cd54
Rmomal <rmomaleisenring@gmail.com>
2020-07-29 20:16:30 +0200

    modified draw_network()

--- R/F_Graphs.R
+++ R/F_Graphs.R
@@ -7,11 +7,12 @@
 #' @param size size of nodes
 #' @param curv edges curvature
 #' @param width maximum width for the edges
-#' @param alpha if TRUE, sets to transparent the edges non-linked to nodes with high betweenness
+#' @param shade if TRUE, shades the edges unlinked to nodes with high betweenness
 #' @param filter_deg selects nodes with a higher degree than filter_deg
-#' @param nb sets the number of nodes selected by thresholding the beetweenness scores
+#' @param btw_rank betweenness rank +1 of highlighted nodes. If set to 1, none are highlighted.
 #' @param layout optional ggraph layout.
 #' @param nodes_label optional labels for nodes.
+#' @param nodes_size size of nodes, possibility to specify a size per group
 #' @param pal optional palette.
 #' @param groupes optional vector seperating the nodes into groupes
 #'
@@ -26,28 +27,27 @@
 #' @importFrom ggplot2 aes theme labs scale_color_manual scale_size_manual
 #' @importFrom dplyr mutate filter
 #' @importFrom viridisLite viridis
-#' @examples adj_matrix= SimCluster(p=30,k=3,dens=0.4, r=50)
-#' draw_network(adj_matrix,"Cluster graph", layout="fr",curv=0.1)
-draw_network<-function(adj_matrix,title="", size=4, curv=0,width=1, alpha=FALSE, filter_deg=FALSE,nb=1,
-                       layout=NULL,nodes_label=NULL,pal="#31374f",  groupes=NULL){
-  p=nrow(adj_matrix)
-  if(is.null(nb)) nb=round(p/8,0)
-  binary=FALSE
-  if(is.null(nodes_label)){ nodes_label=1:p ; bool=TRUE}else{bool=FALSE}
-
+#' @examples adj_matrix= SimCluster(20,2,0.4, 10)
+#' draw_network(adj_matrix,"Cluster graph", layout="fr", shade=TRUE)
+draw_network<-function(adj_matrix,title="", size=4, curv=0,width=1, shade=FALSE, filter_deg=FALSE,btw_rank=2,
+                       layout=NULL,nodes_label=NULL,nodes_size=c(2,5),pal_edges=NULL, pal_nodes=NULL, groupes=NULL){
+  p=nrow(adj_matrix) ; binary=FALSE
+  if(is.null(nodes_label)){ nodes_label=1:p ; nonames=TRUE}else{nonames=FALSE}
   if(sum(unique(adj_matrix))==1) binary=TRUE
-
+  # edges width
+  min.width=ifelse(binary,0,0.1)
+  #edges colour
+  pal_edges <-  ifelse(is.null(pal_edges), viridisLite::viridis(5, option = "C")[c(3,2,4,1)], pal_edges)
+  #betweenness computations
   res<- as_tbl_graph(adj_matrix, directed=FALSE) %>% activate(edges) %>%
     mutate(btw.weights=ifelse(weight==0,0,log(1+1/weight))) %>%
     activate(nodes) %>%
-    mutate(  btw=centrality_betweenness(weights=btw.weights),
-            bool_btw=(btw>sort(btw, decreasing = TRUE)[nb]),bool_deg=(centrality_degree()>0),
-            deg=centrality_degree(), title=title, name=nodes_label
-    )
-  if(!is.null(groupes)){
-    res<-res %>% mutate(groupes=as.factor(groupes))
-  }
-  if(bool){
+    mutate( btw=centrality_betweenness(weights=btw.weights),
+            bool_btw=(btw>sort(btw, decreasing = TRUE)[btw_rank]),
+            bool_deg=(centrality_degree()>0),
+            deg=centrality_degree(), title=title, name=nodes_label )
+  if(!is.null(groupes)) res<-res %>% mutate(groupes=as.factor(groupes))
+  if(nonames){
     res<-res %>% mutate(label=ifelse(bool_btw,name,""))
   }else{
     res<-res %>% mutate(label=ifelse(bool_deg,name,""))
@@ -58,28 +58,26 @@ draw_network<-function(adj_matrix,title="", size=4, curv=0,width=1, alpha=FALSE,
     filter(weight !=0) %>%
     mutate(neibs=edge_is_incident(which(.N()$bool_btw)), title=title)
 
-
-  pal_edges <-  ifelse(is.null(pal), viridisLite::viridis(5, option = "C")[c(3,2,4,1)], pal)
- if(!is.null(groupes)){
-   pal_nodes<-c("#31374f","#adc9e0","#e7bd42")
-   res<-res %>%
-     activate(nodes)  %>%
-     mutate(finalcolor=groupes)
+# define nodes color
+  if(!is.null(groupes)){
+   if(is.null(pal_nodes)) pal_nodes<-c("#31374f","#adc9e0","#e7bd42")
  }else{
-   pal_nodes<-c("black","goldenrod1")
-   res<-res %>%
-     activate(nodes)  %>%
-     mutate(finalcolor=bool_btw)
+   groupes=unlist(res %>%
+     activate(nodes)  %>% dplyr::select(bool_btw) %>% as_tibble())
+   if(is.null(pal_nodes)) pal_nodes<-c("#31374f","#e7bd42")
  }
+  res<-res %>%
+    activate(nodes)  %>%
+    mutate(finalcolor=groupes)
+
+  #draw graph
   set_graph_style(family="sans")
   layout = ifelse(is.null(layout), "circle", layout)
-  g=res %>%
-    ggraph(layout = layout)
-  if(alpha){
+  g=res %>% ggraph(layout = layout)
+  if(shade){ #shading
     g<-g+
       geom_edge_arc(aes(edge_width=weight, alpha=neibs,color = title), strength = curv, show.legend = FALSE) +
       scale_edge_alpha_manual(values=c(0.2,1))
-
   }else{ g<-g+
     geom_edge_arc(aes(edge_width=weight,color = title), strength = curv, show.legend = FALSE)
   }
@@ -87,19 +85,12 @@ draw_network<-function(adj_matrix,title="", size=4, curv=0,width=1, alpha=FALSE,
     geom_node_point(aes(color = finalcolor, size = groupes), show.legend = FALSE) +
     scale_edge_colour_manual(values = pal_edges) +
     scale_color_manual(values = pal_nodes)+
-    scale_size_manual(values = c(2,5,6))+
+    scale_size_manual(values = nodes_size)+
     geom_node_text(aes(label = label), color = "black", size = size) +#,nudge_x = 0.3
-    labs(title = title) + theme(plot.title = element_text(hjust = 0.5))
-
-  if(!binary){ g=g+
-    scale_edge_width_continuous(range=c(0.1,width))
-  }else{
-    g=g+
-      scale_edge_width_continuous(range=c(0,width))
-  }
+    labs(title = title) + theme(plot.title = element_text(hjust = 0.5))+
+    scale_edge_width_continuous(range=c(min.width,width))
 
   return(list(G=g,graph_data=res))
-
 }
 
 
@@ -110,7 +101,7 @@ draw_network<-function(adj_matrix,title="", size=4, curv=0,width=1, alpha=FALSE,
 #' @param allNets tibble resulting of ComparEMtree()
 #' @param curv edges curvature
 #' @param width maximum width for the edges
-#' @param alpha if TRUE, sets to transparent the edges non-linked to nodes with high betweenness
+#' @param shade if TRUE, shades the edges non-linked to nodes with high betweenness
 #' @param Ft Frequency threshold
 #' @param seed optional seed for graph reproductibility
 #' @param nb sets the number of nodes selected by thresholding the beetweenness scores
@@ -139,7 +130,7 @@ draw_network<-function(adj_matrix,title="", size=4, curv=0,width=1, alpha=FALSE,
 #'threemodels=ComparEMtree(Y,X,models=list(1,2,c(1,2)),
 #'m_names=list("1","2","both"),Pt=0.3,S=S, cores=1)
 #'compar_graphs(threemodels)
-compar_graphs<-function(allNets, curv=0.2, width=1, alpha=TRUE,Ft=0,
+compar_graphs<-function(allNets, curv=0.2, width=1, shade=TRUE,Ft=0,
                         nodes_label=NULL,seed=123, nb=3, layout="circle", base_model=NULL){
 
    mods=unique(allNets$model)
@@ -185,7 +176,7 @@ if(is.null(nodes_label)){
     mutate(mod=factor(mod,levels=mods),x=rep(lay$x,nbmod),y=rep(lay$y,nbmod)) %>%
     activate(edges) %>% filter(weight>Ft) %>%
     ggraph(layout="nicely")
-  if(alpha){
+  if(shade){
     plot<-plot+
       geom_edge_arc(aes(edge_width=weight,color=mod, alpha=neibs),strength=curv,show.legend=FALSE)+
       scale_edge_alpha_manual(values=c(0.2,1))
